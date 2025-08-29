87f0c0f2395d7b2dec43265b117c44202a1479fa
Rail Abdullin <>
2025-08-29 13:55:59 +0500

    Ads

---
+++ game-engine/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace gameengine
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ game-engine/Runtime/Core/Ads/RewardType.cs
@@ -0,0 +1,10 @@
+﻿namespace Ads
+{
+    public enum RewardType
+    {
+        None = 0,
+        FreeСoins = 1,
+        DoubleReward = 2,
+        CityManager = 3
+    }
+}
